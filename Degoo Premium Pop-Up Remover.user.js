c55f9aa57863ab7d2be9ee377336b66074927e91
Jabed Miah <84815985+miahj1@users.noreply.github.com>
2023-08-02 23:05:56 -0400

    Update Degoo Premium Pop-Up Remover.user.js
    Added support url and changed code to select every instance of the pop-up.

--- Degoo Premium Pop-Up Remover.user.js
+++ Degoo Premium Pop-Up Remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Degoo Premium Pop-Up Remover
 // @namespace    https://www.jabedmiah.com
-// @version      1.0
+// @version      1.02
 // @description  Removes the Degoo premium pop that restricts usage of the web application.
 // @author       miahj1
 // @match        https://app.degoo.com/files/*
@@ -9,32 +9,19 @@
 // @match        https://app.degoo.com/search*
 // @match        https://app.degoo.com/albums/*
 // @icon         https://www.google.com/s2/favicons?domain=degoo.com
+// @supportURL   https://github.com/miahj1/Degoo-Premium-Pop-Up-Remover/issues
 // @license MIT
 // ==/UserScript==
 
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
 
 function check(changes, observer) {
-    const popUpElements = {
-        elementOne: document.querySelector(".ng-tns-c38-7.ng-star-inserted"),
-        elementTwo: document.querySelector(".ng-tns-c38-6.ng-star-inserted"),
-        elementThree: document.querySelector(".ng-tns-c38-3.ng-star-inserted"),
-        elementFour: document.querySelector(".ng-tns-c38-8.ng-star-inserted")
-    };
+    const popUpElement = document.getElementsByTagName("app-dialog")[0];
 
-    if (popUpElements.elementOne) {
+    if (popUpElement) {
         observer.disconnect();
-        popUpElements.elementOne.style.display = "none"
-    } else if (popUpElements.elementTwo) {
-        observer.disconnect();
-        popUpElements.elementTwo.style.display = "none"
-    } else if (popUpElements.elementThree) {
-        observer.disconnect();
-        popUpElements.elementThree.style.display = "none"
-    } else if (popUpElements.elementFour){
-        observer.disconnect();
-        popUpElements.elementFour.style.display = "none"
+        popUpElement.style.display = "none";
     }
 
-    document.body.style = 'overflow: unset'
-};
+    document.body.style = 'overflow: unset';
+}
